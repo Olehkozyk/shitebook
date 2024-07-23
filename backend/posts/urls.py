74e6972884fbc0d4ecc93f6ebc5266ca46094718
Oleh Kozyk <oleh.kozyk@beetroot.se>
2024-07-23 18:00:32 +0300

    update project settings

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import PostsListAPIView, PostItemAPIView
 urlpatterns = [
-    path('/', PostsListAPIView.as_view(), name='posts'),
-    path('/<str:id>', PostItemAPIView.as_view(), name='post'),
+    path('', PostsListAPIView.as_view(), name='posts'),
+    path('<str:id>', PostItemAPIView.as_view(), name='post'),
 ]
--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from .views import UsersListAPIView
 urlpatterns = [
-    path('/', UsersListAPIView.as_view(), name='users'),
+    path('', UsersListAPIView.as_view(), name='users'),
 ]
